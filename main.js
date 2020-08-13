dca919d0cc24a2197ef48cfdffeddda0f482832e
vivians-y <55055539+vivians-y@users.noreply.github.com>
2020-08-12 21:35:40 -0500

    Item page visuals

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-document.onscroll=(function () {
-    if (document.scrollingElement.scrollTop>1) {
+document.onscroll = (function () {
+    console.log("I just ran");
+
+    if (document.scrollingElement.scrollTop > 1) {
         document.querySelector("header").classList.remove("header-transparent");
     } else {
         document.querySelector("header").classList.add("header-transparent");
     }
 });
+
+
